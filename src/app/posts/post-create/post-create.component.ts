f261882db6c8499653137fae2df3c1c1d4f4d53a
haodant <haodan_tan@ultimatesoftware.com>
2020-01-18 18:25:01 -0500

    image update completed

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { PostsService } from "../posts.service";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Post } from "../post.model";
-import { mimeType } from "./mime-type.validator"
+import { mimeType } from "./mime-type.validator";
 
 @Component({
   selector: "app-post-create",
@@ -29,7 +29,10 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)]
       }),
       content: new FormControl(null, { validators: [Validators.required] }),
-      image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has("postId")) {
@@ -41,12 +44,14 @@ export class PostCreateComponent implements OnInit {
           this.post = {
             id: postData._id,
             title: postData.title,
-            content: postData.content
+            content: postData.content,
+            imagePath: postData.imagePath
           };
-        });
-        this.form.setValue({
-          title: this.post.title,
-          content: this.post.content
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content,
+            image: this.post.imagePath
+          });
         });
       } else {
         this.mode = "create";
@@ -57,12 +62,12 @@ export class PostCreateComponent implements OnInit {
 
   onImagePicked(event: Event) {
     const file = (event.target as HTMLInputElement).files[0];
-    this.form.patchValue({image: file});
-    this.form.get('image').updateValueAndValidity();
+    this.form.patchValue({ image: file });
+    this.form.get("image").updateValueAndValidity();
     const reader = new FileReader();
     reader.onload = () => {
-        this.imgPreview = reader.result as string;
-    }
+      this.imgPreview = reader.result as string;
+    };
     reader.readAsDataURL(file);
   }
 
@@ -71,15 +76,21 @@ export class PostCreateComponent implements OnInit {
       return;
     }
     if (this.mode == "create") {
-      this.postsService.addPosts(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
     } else {
       this.postsService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
       );
     }
 
     this.form.reset();
   }
 }
+
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,76 +1,108 @@
-import { Post } from './post.model';
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Post } from "./post.model";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class PostsService {
-    private posts: Post[] = [];
-    private postsUpdated = new Subject<Post[]>();
-    private postUrl = "http://localhost:3000/api/posts/";
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+  private postUrl = "http://localhost:3000/api/posts/";
 
-    constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-    getPosts() {
-        this.http.get<{ message: string, posts: any }>(
-            this.postUrl
-            )
-            .pipe(map(postData => {
-                return postData.posts.map(post => {
-                    return {
-                        title: post.title,
-                        content: post.content,
-                        id: post._id
-                    }
-                })
-            }))
-            .subscribe((posts) => {
-                this.posts = posts;
-                this.postsUpdated.next([...this.posts]);
-            })
-    }
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: any }>(this.postUrl)
+      .pipe(
+        map(postData => {
+          return postData.posts.map(post => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+              imagePath: post.imagePath
+            };
+          });
+        })
+      )
+      .subscribe(posts => {
+        this.posts = posts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
 
-    getPostUpdatedListener() {
-        return this.postsUpdated.asObservable();
-    }
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
 
-    getPost(id: string) {
-        return this.http.get<{_id: string, title: string, content: string}>(this.postUrl + id);
-    }
+  getPost(id: string) {
+    return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>(
+      this.postUrl + id
+    );
+  }
 
-    updatePost(id: string, title: string, content: string) {
-        const post: Post = {id: id, title: title, content: content};
-        this.http.put(this.postUrl + id, post)
-            .subscribe(response => {
-                const updatePosts = [...this.posts];
-                const oldPostIndex = updatePosts.findIndex(p => p.id === post.id);
-                updatePosts[oldPostIndex] = post;
-                this.posts = updatePosts;
-                this.postsUpdated.next([...this.posts]);
-                this.router.navigate(["/"]);
-            });
-    }
+  updatePost(id: string, title: string, content: string, image: File | string) {
+      let postData: Post | FormData;
+      if (typeof image === 'object') {
+        postData = new FormData();
+        postData.append('id', id);
+        postData.append("title", title);
+        postData.append("content", content);
+        postData.append("image", image, title);
+      } else {
+        postData = {
+            id: id,
+            title: title,
+            content: content,
+            imagePath: image
+          };
+      }
+    this.http.put(this.postUrl + id, postData)
+      .subscribe(response => {
+      const updatePosts = [...this.posts];
+      const oldPostIndex = updatePosts.findIndex(p => p.id === id);
+      const post: Post = {
+        id: id,
+        title: title,
+        content: content,
+        imagePath: ""//response.imagePath
+      };
+      updatePosts[oldPostIndex] = post;
+      this.posts = updatePosts;
+      this.postsUpdated.next([...this.posts]);
+      this.router.navigate(["/"]);
+    });
+  }
 
-    addPosts(title: string, content: string) {
-        const post: Post = {id: null, title: title, content: content};
-        this.http.post<{message: string, postId: string}>(this.postUrl, post)
-            .subscribe((responseData) => {
-                const postId = responseData.postId;
-                post.id = postId;
-                this.posts.push(post);
-                this.postsUpdated.next([...this.posts]);
-                this.router.navigate(["/"]);
-            });
-    }
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title);
 
-    deletePost(postId: string) {
-        this.http.delete(this.postUrl + postId)
-            .subscribe(() => {
-                const updatedPosts = this.posts.filter(post => post.id != postId);
-                this.posts = updatedPosts;
-                this.postsUpdated.next([...this.posts]);
-            })
-    }
+    this.http
+      .post<{ message: string; post: Post }>(this.postUrl, postData)
+      .subscribe(responseData => {
+        const post: Post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath
+        };
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete(this.postUrl + postId).subscribe(() => {
+      const updatedPosts = this.posts.filter(post => post.id != postId);
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
 }
